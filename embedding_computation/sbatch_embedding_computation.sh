9016e659c3ed3248719c3a49effb7e01574d785b
Alaa ALMUTAWA <almutawa@localhost.lip6.fr>
2024-08-07 12:42:07 +0200

    [m1] sample of a batch job

---
+++ embedding_computation/sbatch_embedding_computation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hytrel_inference
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=a100_7g.80gb:1
+#SBATCH --time=30000
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+module purge                        # Environment cleanup
+module load python/anaconda3 # Loading of anaconda3 module
+module load cuda/11.1 ##loading cuda module 
+module load gcc/10 ##loading gcc compiler 
+eval "$(conda shell.bash hook)"     # Shell initialization to use conda
+conda activate hytrel_env                # Activation your python environment
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
+python compute_embeddings.py
