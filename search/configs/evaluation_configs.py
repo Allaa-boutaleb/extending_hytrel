a57e4175b7d5b0b243ed5dc5990e92082a3f4248
Alaa Almutawa <alaaalmutawa@Alaas-MacBook-Pro.local>
2024-08-14 10:23:45 +0200

    [m4] ...

--- search/configs/evaluation_configs.py
+++ search/configs/evaluation_configs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 input = {
-    "benchmark": "nextiajd_s",
-    "query_set": "set1",
-    "candidates": ['/Users/alaaalmutawa/Documents/Thesis/hytrel/hypergraph-tabular-lm/inference/inference/nextiajd/testbedS/test/postprocessing/filtered_4.pkl'],
+    "benchmark": "webtable",
+    "query_set": "",
+    "candidates": ['/home/almutawa/inference/search/lakebench/webtable/candidates_0.pkll'],
     "table_search_task": "join", 
     "search_method": 'faiss',
-    "type": "filtering",
-    "comparsion":['rerank_4']
+    "type": "search",
+    "comparsion":['faiss']
 }
 
 benchmarks = {
@@ -21,7 +21,8 @@ k = {
     'nextiajd_s': 10,
     'nextiajd_m': 10,
     'webtables_small': 20,
-    'webtables_small_var2': 20
+    'webtables_small_var2': 20,
+    'webtables':20
 }
 
 gt_paths = {'santos':'/home/almutawa/starmie/data/santos/santosUnionBenchmark.pickle',
@@ -31,9 +32,10 @@ gt_paths = {'santos':'/home/almutawa/starmie/data/santos/santosUnionBenchmark.pi
       'nextiajd_s': {'set1':'/Users/alaaalmutawa/Documents/Thesis/nextiajd/testbedS/warpgate/join_dict_testbedS_warpgate.pkl','set2':'/Users/alaaalmutawa/Documents/Thesis/nextiajd/testbedS/warpgate_non-numerical/join_dict_testbedS_warpgate_non-numerical.pkl','set3':'/Users/alaaalmutawa/Documents/Thesis/nextiajd/testbedS/10_non-numerical/join_dict_testbedS_10_non-numerical.pkl'},
       'nextiajd_m': '/Users/alaaalmutawa/Documents/Thesis/nextiajd/testbedM/warpgate/join_dict_testbedM_warpgate.pkl',
        'webtables_small':'/Users/alaaalmutawa/Documents/Thesis/lakebench/join/webtable/join_dict_webtable_small.pkl',
-        'webtables_small_var2':'/Users/alaaalmutawa/Documents/Thesis/lakebench/join/webtable/small_var2/join_dict_webtable_small_var2.pkl'
+        'webtables_small_var2':'/Users/alaaalmutawa/Documents/Thesis/lakebench/join/webtable/small_var2/join_dict_webtable_small_var2.pkl',
+        'webtables':'/home/almutawa/inference/inference/lakebench/webtable/join_dict_final.pkl'
 }
 
 output = {
-    'path': '/Users/alaaalmutawa/Documents/Thesis/hytrel/hypergraph-tabular-lm/inference/inference/nextiajd/testbedS/postprocessing/evaluation/set1/charts_excel/rerank',
+    'path': '/Users/alaaalmutawa/Documents/Thesis/hytrel/hypergraph-tabular-lm/inference/inference/lakebench/webtable/evaluation',
 }
